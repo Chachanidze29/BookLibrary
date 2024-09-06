36521cbef82fc7e0e069aceae6932a47188534f3
Chachanidze29 <chachanidze29m@gmail.com>
2024-09-06 15:06:35 +0400

    remove obsolete prop

--- resources/js/Pages/Authors/Show.tsx
+++ resources/js/Pages/Authors/Show.tsx
@@ -2,21 +2,15 @@ import { Head } from '@inertiajs/react';
 
 import AuthorDetails from '@/Components/AuthorDetails';
 import MainLayout from '@/Layouts/MainLayout';
-import { Author, Book } from '@/types/model';
+import { Author } from '@/types/model';
 
-export default function Show({
-    author,
-    books,
-}: {
-    author: Author;
-    books?: Book[];
-}) {
+export default function Show({ author }: { author: Author }) {
     return (
         <MainLayout>
             <Head title={author.name} />
 
             <div className="container flex flex-grow flex-col">
-                <AuthorDetails author={author} books={books} />
+                <AuthorDetails author={author} />
             </div>
         </MainLayout>
     );
